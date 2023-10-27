4025190782b6afea918c8a313539b64e2253c9c9
ghost-sys-coder <juniorbeast177@gmail.com>
2023-10-27 16:50:07 +0300

    Fixing the streaming error in the Cards Wrapper

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,9 @@
 import React, { Suspense } from 'react';
 import { lusitana } from '@/app/ui/fonts';
-import Cards, { Card } from '@/app/ui/dashboard/cards';
+import Cards from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
 
 type Props = {}
@@ -17,7 +17,9 @@ const page = async (props: Props) => {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <Cards />
+                <Suspense fallback={<CardsSkeleton />}>
+                    <Cards />
+                </Suspense>
             </div>
             <div className="grid mt-6 grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <Suspense fallback={<RevenueChartSkeleton/>}>
